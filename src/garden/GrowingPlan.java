e8485305ee9ffd5996430473085647457ab467bb
xrz <zxr88826@gmail.com>
2016-02-22 23:09:09 -0800

    check in

--- src/garden/GrowingPlan.java
+++ src/garden/GrowingPlan.java
@@ -2,14 +2,20 @@ package garden;
 
 public class GrowingPlan {
 	private static int frequency;
+	public static int period1 = 240;
+	public static int period2 = 480;
+	public static int highFreq = 12;
+	public static int mediumFreq = 8;
+	public static int lowFreq = 6;
+	
 	
 	public static int frequency(long growthPeriod) {
-		if (growthPeriod >= 0 && growthPeriod < 240) {
-			frequency = 12;
-		} else if (growthPeriod >= 240 && growthPeriod < 480) {
-			frequency = 8;
+		if (growthPeriod >= 0 && growthPeriod < period1) {
+			frequency = highFreq;
+		} else if (growthPeriod >= period1 && growthPeriod < period2) {
+			frequency = mediumFreq;
 		} else {
-			frequency = 6;
+			frequency = lowFreq;
 		}
 		return frequency;
 	}
